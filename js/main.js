1f62ba6ee529b59d7ba251e30132969b8d9bb9e6
Michael Mansy <michael.mansy@yahoo.ca>
2026-03-16 19:50:40 -0500

    Added event listeners for interior and exterior buttons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const topBar = document.querySelector('#top-bar');
 
+const exteriorColorSelection = document.querySelector('#exterior-buttons');
+const interiorColorSelection = document.querySelector('#interior-buttons');
+
+const exteriorImage = document.querySelector('#exterior-image');
+const interiorImage = document.querySelector('#interior-image');
+
 
 // Handle the top bar on scroll
 const handleScroll = () => {
@@ -9,5 +15,26 @@ const handleScroll = () => {
 };
 
 
+
+// handle the color selections
+const handleColorButtonClick = (event) =>{
+    let button;
+
+    if(event.target.tagName === 'IMG'){
+        button = event.target.closest('button');
+    } else if(event.target.tagName === 'BUTTON'){
+        button = event.target;
+    }
+
+    if(button){
+        const buttons = event.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn) => btn.classList.remove('btn-selected'));
+        button.classList.add('btn-selected');
+    }
+}
+
 // Event Listeners
 window.addEventListener('scroll', () => requestAnimationFrame(handleScroll));
+
+exteriorColorSelection.addEventListener('click', handleColorButtonClick);
+interiorColorSelection.addEventListener('click', handleColorButtonClick);
